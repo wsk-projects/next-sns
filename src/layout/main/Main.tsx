8aafe8597c6f8d57647cc3fc746cc8c1ba6fb0ce
wsk0715 <leindey@gmail.com>
2025-04-16 22:14:25 +0900

    :recycle:refactor: Container 컴포넌트 추가
    - 기본 레이아웃에 Container 컴포넌트 적용

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -1,3 +1,9 @@
+import Container from "@/components/container/Container";
+
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main className="flex-1">{children}</main>;
+  return (
+    <Container as="main" className="flex-1">
+      {children}
+    </Container>
+  );
 }
--- src/layout/nav/Nav.tsx
+++ src/layout/nav/Nav.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { useBreakpoint } from "@/hooks/useBreakpoint";
-
+import Container from "@/components/container/Container";
 export default function Navigation() {
   const isDesktop = useBreakpoint("lg");
 
-  return <nav>{isDesktop ? <NavDesktop /> : <NavMobile />}</nav>;
+  return (
+    <Container as="nav" className="min-h-8 bg-neutral-500 text-white">
+      {isDesktop ? <NavDesktop /> : <NavMobile />}
+    </Container>
+  );
 }
 
 const NavDesktop = () => {
