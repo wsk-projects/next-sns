fb4e6ea635666631c3ea2f6558f18ce15f785b1e
wsk0715 <leindey@gmail.com>
2025-04-17 00:59:53 +0900

    :art:design: 헤더 레이아웃 구현

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,43 @@
-import Container from "@/components/container/Container";
+import Wrapper from "@/components/common/container/Wrapper";
+import { Title } from "@/components/common/content/Title";
+import Button from "@/components/common/button/Button";
+import HeaderBackButton from "../../components/ui/button/BackButton";
+import Container from "@/components/common/container/Container";
+import { clsx } from "clsx";
+import { Menu } from "lucide-react";
 
-export default function Header() {
+export default function Header(): React.ReactNode {
   return (
-    <Container as="header" className="min-h-16 bg-neutral-800 text-white">
-      Header
+    <HeaderWrapper>
+      <HeaderBackButton />
+      <Title.h1>페이지 제목</Title.h1>
+      <HeaderActions />
+    </HeaderWrapper>
+  );
+}
+
+function HeaderWrapper({ children }: { children: React.ReactNode }): React.ReactNode {
+  return (
+    <Container
+      as="header"
+      className={clsx(
+        "flex items-center justify-between",
+        "w-full min-h-16 border-b",
+        "fixed top-0 left-0 z-10",
+        "bg-neutral-800 text-white"
+      )}
+    >
+      {children}
     </Container>
+  );
+}
+
+function HeaderActions(): React.ReactNode {
+  return (
+    <Wrapper className="flex items-center gap-2">
+      <Button>
+        <Menu size={32} />
+      </Button>
+    </Wrapper>
   );
 }
